6033931d9fd7b1ef3d23f58d1dec4d592841a111
Yashupadhyaya <uyash73@gmail.com>
2025-01-15 13:47:14 +0530

    changes

---
+++ newgo/new.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/golang/mock/gomock"
+)
+
+func OldMockMatcher(ctrl *gomock.Controller) *MockMatcher {
+	mock := &MockMatcher{ctrl: ctrl}
+	mock.recorder = &MockMatcherMockRecorder{mock}
+	return mock
+}
